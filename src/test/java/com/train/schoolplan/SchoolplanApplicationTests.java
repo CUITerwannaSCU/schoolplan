7476f9c50b003cde4809343cd3149dfbe7394fea
86182 <1691441008>
2022-05-17 14:16:02 +0800

    Initial commit

---
+++ src/test/java/com/train/schoolplan/SchoolplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.schoolplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
